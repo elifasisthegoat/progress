0da032bfbda6706e5780efb7f423acd76d170e44
elifasisthegoat <eleomiengelbrechtbusiness@gmail.com>
2023-12-20 11:27:08 +0200

    new code

---
+++ pseudocode.py
@@ -0,0 +1,11 @@
+#prompt user to input num1
+num1 = float(input("num1= "))
+
+#prompt user to input num2
+num2 = float(input("num2="))
+
+#define the sum of num1 and num2
+sum = float(num1 + num2)
+
+#output the result
+print(sum)
